c060db15d6a0d2f31fc96066a33b7e03b375a939
KeziahMoselle <keziah.moselle@gmail.com>
2018-12-09 22:12:10 +0100

    feat: Improve the HTML file output

--- src/exports/html.js
+++ src/exports/html.js
@@ -1,9 +1,42 @@
 import download from 'downloadjs'
 
 export default function (starredRepos, username) {
-  let data = `<h1>${username}'s starred repositories :</h1>`
+  const header = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>${username}'s starred repositories</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" />
+    </head>
+    <body class="container">
+      <h1 style="margin-top: 40px; margin-bottom: 10px;">${username}'s</h1>
+      <h2 style="">Starred repositories</h2>
+      <table class="u-full-width">
+        <thead>
+          <tr>
+            <th>Repository</th>
+            <th>Stars</th>
+          </tr>
+        </thead>
+        <tbody>
+  `
+  let body = ''
   starredRepos.forEach(repo => {
-    data += `<p><a href="${repo.html_url}">${repo.name} (${repo.stars}⭐)</a></p>`
+    body +=
+    `<tr>
+      <td>
+        <a href="${repo.html_url}">${repo.name}</a>
+      </td>
+      <td>${repo.stars}⭐</td>
+    </tr>`
   })
+  const footer = ` 
+        </tbody>
+      </table>
+    </body>
+    </html>
+  `
+  const data = `${header} ${body} ${footer}`
   download(data, `${username}-starred.html`, 'text/html')
 }
