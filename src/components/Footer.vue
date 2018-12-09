82e3edf54c127d1bcf360139533c1f5289db869f
KeziahMoselle <keziah.moselle@gmail.com>
2018-12-09 08:47:28 +0100

    fix: footer scroll & add icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer>
-    <v-layout wrap justify-space-between class="white--text py-3 px-4" black>
-      <span>Made by <a href="https://github.com/KeziahMoselle">KeziahMoselle</a></span>
+    <v-layout wrap justify-space-between align-center fill-height class="white--text px-5" black>
+      <span> <v-icon color="white">change_history</v-icon> Made by <a href="https://github.com/KeziahMoselle">KeziahMoselle</a></span>
       <span>Illustrations by <a href="https://undraw.co/illustrations">Undraw.co</a></span>
     </v-layout>
   </v-footer>
