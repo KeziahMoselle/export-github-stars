c299956395d70b13893be2928224435374ecf370
KeziahMoselle <keziah.moselle@gmail.com>
2018-12-10 06:18:47 -0800

    feat(footer): UI improvements

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,30 +1,35 @@
 <template>
-  <v-footer class="pa-4" dark>
-    <div>
-      <v-icon color="white">change_history</v-icon> Made by <a href="https://github.com/KeziahMoselle">KeziahMoselle</a>
-    </div>
-    <v-spacer></v-spacer>
-    <div>
-      Illustrations by <a href="https://undraw.co/illustrations">Undraw.co</a>
-    </div>
+  <v-footer height="auto" color="black">
+    <v-layout justify-center wrap>
+      <template v-for="link in links">
+        <v-btn :key="link.title" :href="link.href" color="white" flat round>
+          {{ link.title }}
+        </v-btn>
+      </template>
+    </v-layout>
   </v-footer>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  data () {
+    return {
+      links: [
+        {
+          title: 'Author',
+          href: 'https://github.com/KeziahMoselle'
+        },
+        {
+          title: 'Source Code',
+          href: 'https://github.com/KeziahMoselle/export-github-stars'
+        },
+        {
+          title: 'Illustrations',
+          href: 'https://undraw.co/illustrations'
+        }
+      ]
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-  a {
-    color: white;
-    text-decoration: none;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-
-</style>
